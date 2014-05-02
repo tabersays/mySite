a1862b01527a2c80e44c7449025eab667adcdddc
tabersays <tfitzgerald3@mail.csuchico.edu>
2014-05-01 18:50:27 -0700

    Pretty up site
    Added image resizing

--- public_html/myjavascript.js
+++ public_html/myjavascript.js
@@ -5,27 +5,39 @@
     Description:
         allows for dynamic changes such as highlighting and body switching and css switching
 */
+function dayHeader() {
+    var day = new Date();
+    if(day.getDay() === 3)
+        {
+            document.getElementById("header").innerHTML="Happy Commando Wednesday!";
+        }
+    return;
+}
 function adjustStyle(width) {
     width = parseInt(width);
     if (width < 700) {
         $("#styleType").attr("href", "mobile.css");
         $(".notSmall").hide();
-        return;
+        
     }
     else if (width > 700 && width < 1100) {
         $("#styleType").attr("href", "small.css");
         $(".notSmall").show();
-        return;
+        
     }
     else {
         $("#styleType").attr("href", "mystyle.css"); 
         $(".notSmall").show();
-        return;
+        
     }
+    return;
 }
+
 $(document).ready(function(){
     
+    dayHeader();
     adjustStyle($(this).width());
+    
     $(window).resize(function() {
         adjustStyle($(this).width());
     });
