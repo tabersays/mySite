4386add6e0e338d9bbd4338440051e61185ac6f9
Taber Storm Fitzgerald <tfitzgerald3@mail.csuchico.edu>
2014-12-28 17:26:17 -0800

    Updates

--- bossy.slider.js
+++ bossy.slider.js
@@ -1,15 +1,23 @@
 /*This is a slider widget created in angular as part of the BossyUI widgets.
  * The easiest way to use the slider is to include it in your HTML and then
  * create a tag <bossy-slider></bossy-slider>. This widget take in several
- * ways to customize. Currently it takes max, min, width, buttoncolor, barfillcolor, baremptycolor and
- * orientation. ex.
+ * ways to customize. List of customizations available.
+ * max              defaults to 100
+ * min              defaults to 1
+ * width            defaults to 250px
+ * barfillcolor     defaults to darkblue: must be passed as hexadecimal color format #000000
+ * baremptycolor    defaults to lightgrey
+ * buttoncolor      defaults to red
+ * step             defaults to red
+ * orientation      defaults to horizontal
+ * ex.
  * <bossy-slider max="20" min="-5" orientation="vertical"></bossy-slider>*/
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var app = angular.module('bossy.slider', []);
 app.controller('SliderController', ['$scope', function ($scope) {
     //these are our default values and are the variables that can be changed by user of our widgets
-    $scope.max = 10;
+    $scope.max = 100;
     $scope.value = 0;
     $scope.min = 1;
     $scope.fillWidth = 0;
@@ -24,12 +32,16 @@ app.controller('SliderController', ['$scope', function ($scope) {
     $scope.newYCord = 0;
     $scope.orientation = false;
     $scope.butSize = 15;
+    $scope.barfillcolor = "#0000FF";
+    $scope.baremptycolor = "#D3D3D3";
+    $scope.buttoncolor = "#FF0000";
 
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /*makeBar()
-     * This creates the initial graphic of the slider and ensures it is in the correct order*/
+     * This creates the initial graphic of the slider and ensures it is in the correct order
+     * CC = 4 */
     $scope.makeBar = function () {
         //button should show up in the middle now or close to if uneven
         $scope.value = parseInt(($scope.max + $scope.min) / 2);
@@ -50,7 +62,8 @@ app.controller('SliderController', ['$scope', function ($scope) {
 
     /*increase()
      * This checks bounds when attempting to increase the value and moves the position
-     * of the slider button and updates the value.*/
+     * of the slider button and updates the value.
+     * CC = 2*/
     $scope.increase = function () {
         if ($scope.value < $scope.max) {
             $scope.value = $scope.value + 1;
@@ -63,18 +76,21 @@ app.controller('SliderController', ['$scope', function ($scope) {
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /*butIncrease()
-     * This function allows the slider to increase in increments.*/
+     * This function allows the slider to increase in increments.
+     * CC = 1*/
     $scope.butIncrease = function () {
         var i = 0;
         for (i = 0; i < $scope.step; i++) {
             $scope.increase();
         }
+        return;
     }
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /*decrease()
      * This checks bounds when attempting to decrease the value and moves the position
-     * of the slider button and updates the value.*/
+     * of the slider button and updates the value.
+     * CC = 2*/
     $scope.decrease = function () {
         if ($scope.value > $scope.min) {
             $scope.value = $scope.value - 1;
@@ -87,32 +103,30 @@ app.controller('SliderController', ['$scope', function ($scope) {
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /*butDecrease()
-     * This function allows the slider to decrease in increments*/
+     * This function allows the slider to decrease in increments
+     * CC = 1*/
     $scope.butDecrease = function () {
         var i = 0;
         for (i = 0; i < $scope.step; i++) {
             $scope.decrease();
         }
+        return;
     }
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /*keyBind($event)
-     * This function is to bind the decrease and increase function with the arrow keys*/
+     * This function is to bind the decrease and increase function with the arrow keys
+     * CC = 5*/
     $scope.keyBind = function (ev) {
         $scope.pressed = ev.which;
         //If arrow key(Left or Down) is pressed then call the decrease() function to decrease the value.
         if ($scope.pressed === 37 || $scope.pressed === 40) {
-            var i = 0;
-            for (i = 0; i < $scope.step; i++) {
-                $scope.decrease();
-            }
+            $scope.butDecrease();
 
         }
         //same as above but for Up or Right to increase the value.
         if ($scope.pressed === 38 || $scope.pressed === 39) {
-            for (i = 0; i < $scope.step; i++) {
-                $scope.increase();
-            }
+            $scope.butIncrease();
 
         }
         return;
@@ -120,20 +134,22 @@ app.controller('SliderController', ['$scope', function ($scope) {
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /*greyClick()
-     * This function is to allow the value to be changed when clicking on the bar*/
+     * This function is to allow the value to be changed when clicking on the bar
+     * CC = 1*/
     $scope.greyClick = function (event) {
         //When click on the empty bar the bar will increase
-        $scope.increase();
+        $scope.butIncrease();
 
         return;
     };
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /*barClick()
-     * This function is to allow the value to be changed when clicking on the bar*/
+     * This function is to allow the value to be changed when clicking on the bar
+     * CC = 1*/
     $scope.barClick = function (event) {
         //When click on the Filled up color side the bar will decrease
-        $scope.decrease();
+        $scope.butDecrease();
 
         return;
     };
@@ -141,7 +157,8 @@ app.controller('SliderController', ['$scope', function ($scope) {
 
     /*drag($event)
      * This function allows the button to drag by finding its location then checks it against its original location
-     * and if it is distance is greater than the size of a barpiece update the graphic and value*/
+     * and if it is distance is greater than the size of a barpiece update the graphic and value
+     * CC = 9*/
     $scope.drag = function (event) {
 
         //grab the mouse location
@@ -158,11 +175,11 @@ app.controller('SliderController', ['$scope', function ($scope) {
                 else {
                     //change the location of the slider after enough movement
                     $scope.newYCord = y;
-                    if (($scope.newYCord - $scope.yCord) > $scope.barPiece / 2) {
+                    while (($scope.newYCord - $scope.yCord) > $scope.barPiece / 2) {
                         $scope.yCord += $scope.barPiece;
                         $scope.decrease();
                     }
-                    if (($scope.newYCord - $scope.yCord) < -($scope.barPiece / 2)) {
+                    while (($scope.newYCord - $scope.yCord) < -($scope.barPiece / 2)) {
                         $scope.yCord -= $scope.barPiece;
                         $scope.increase();
                     }
@@ -176,11 +193,11 @@ app.controller('SliderController', ['$scope', function ($scope) {
                 else {
                     //change the location of the slider after enough movement
                     $scope.newXCord = x;
-                    if (($scope.newXCord - $scope.xCord) > $scope.barPiece / 2) {
+                    while (($scope.newXCord - $scope.xCord) > $scope.barPiece / 2) {
                         $scope.xCord += $scope.barPiece;
                         $scope.increase();
                     }
-                    if (($scope.newXCord - $scope.xCord) < -($scope.barPiece / 2)) {
+                    while (($scope.newXCord - $scope.xCord) < -($scope.barPiece / 2)) {
                         $scope.xCord -= $scope.barPiece;
                         $scope.decrease();
                     }
@@ -192,27 +209,27 @@ app.controller('SliderController', ['$scope', function ($scope) {
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /*down()
-     * This function logs when the mouse is down*/
+     * This function logs when the mouse is down
+     * CC = 1*/
     $scope.down = function () {
         $scope.newXCord = 0;
         $scope.xCord = 0;
         $scope.newYCord = 0;
         $scope.yCord = 0;
         $scope.isMouseDown = 1;
-        //$scope.butSize = 20;
         return;
     };
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /*down()
-     * This function logs when the mouse is up*/
+     * This function logs when the mouse is up
+     * CC = 1*/
     $scope.up = function () {
         $scope.newXCord = 0;
         $scope.xCord = 0;
         $scope.newYCord = 0;
         $scope.yCord = 0;
         $scope.isMouseDown = 0;
-        //$scope.butSize = 15;
         return;
     };
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -229,85 +246,89 @@ app.directive('bossySlider', function ($compile) {
         /*link: function:
          * This allows us to pull in the settings the programmer wants for the slider and set things correctly
          * it also initializes the slider and adds the correct orientation template to the DOM*/
-        link: {
-            pre: function (scope, iElem, iAttr) {
+        link: function (scope, iElem, iAttr) {
 
-                //checks to see if there is a max attribute
-                if (iAttr.max) {
-                    scope.max = parseInt(iAttr.max);
-                    if (scope.max === NaN) {
-                        scope.max = 10;
-                    }
+            //checks to see if there is a max attribute
+            if (iAttr.max) {
+                scope.max = parseInt(iAttr.max);
+                if (scope.max === NaN) {
+                    scope.max = 10;
                 }
-                //checks to see if there is a min attribute
-                if (iAttr.min) {
-                    scope.min = parseInt(iAttr.min);
-                    if (scope.min === NaN) {
-                        scope.min = 1;
-                    }
+            }
+            //checks to see if there is a min attribute
+            if (iAttr.min) {
+                scope.min = parseInt(iAttr.min);
+                if (scope.min === NaN) {
+                    scope.min = 1;
                 }
-                //checks for bar color customization
-                scope.barfillcolor = "#0000FF";
-                if (iAttr.barfillcolor) {
-                    var pattern = /^#[0-9a-fA-F]{6}$/; //currently accepts lower case a-f
-                    if (pattern.test(iAttr.barfillcolor)) {
-                        scope.barfillcolor = iAttr.barfillcolor;
-                    }
+            }
+            //checks for bar color customization
+            if (iAttr.barfillcolor) {
+                var pattern = /^#[0-9a-fA-F]{6}$/; //currently accepts lower case a-f
+                if (pattern.test(iAttr.barfillcolor)) {
+                    scope.barfillcolor = iAttr.barfillcolor;
                 }
-                //checks for empty bar color customization
-                scope.baremptycolor = "#D3D3D3";
-                if (iAttr.baremptycolor) {
-                    var pattern = /^#[0-9a-fA-F]{6}$/; //currently accepts lower case a-f
-                    if (pattern.test(iAttr.baremptycolor)) {
-                        scope.baremptycolor = iAttr.baremptycolor;
-                    }
-                }
+            }
+            //checks for empty bar color customization
 
-                scope.buttoncolor = "#FF0000";
-                if (iAttr.buttoncolor) {
-                    var pattern = /^#[0-9a-fA-F]{6}$/; //currently accepts lower case a-f
-                    if (pattern.test(iAttr.buttoncolor)) {
-                        scope.buttoncolor = iAttr.buttoncolor;
-                    }
+            if (iAttr.baremptycolor) {
+                var pattern = /^#[0-9a-fA-F]{6}$/; //currently accepts lower case a-f
+                if (pattern.test(iAttr.baremptycolor)) {
+                    scope.baremptycolor = iAttr.baremptycolor;
                 }
-                //find the step size for button clicks
-                if (iAttr.step) {
-                    scope.step = iAttr.step;
+            }
+
+
+            if (iAttr.buttoncolor) {
+                var pattern = /^#[0-9a-fA-F]{6}$/; //currently accepts lower case a-f
+                if (pattern.test(iAttr.buttoncolor)) {
+                    scope.buttoncolor = iAttr.buttoncolor;
                 }
-                //find the preferred total width to use for the slider
-                if (iAttr.width) {
-                    scope.barWidth = iAttr.width;
-                    scope.barPiece = (scope.barWidth / (scope.max - scope.min));
+            }
+            //find the step size for button clicks
+            if (iAttr.step) {
+                scope.step = iAttr.step;
+            }
+            //find the preferred total width to use for the slider
+            if (iAttr.width) {
+                scope.barWidth = iAttr.width;
+                scope.barPiece = (scope.barWidth / (scope.max - scope.min));
+            }
+            else {
+                scope.barPiece = (scope.barWidth / (scope.max - scope.min));
+            }
+            //checks to see if there is a orientation attribute if there is set our template to the vertical template
+            if (iAttr.orientation) {
+                if ('vertical' === iAttr.orientation) {
+                    scope.orientation = true;
+                    myTemplate = '<div onselectstart="return false;" ondragstart="return false;"ng-mouseleave="up()" ng-mousemove="drag($event)">' +
+                    '<div ng-mousemove="drag($event)" ng-mouseup="up()" ng-click="greyClick()"style="cursor:pointer;margin-left:9px;width:5px;height:{{barPiece * emptWidth}}px;background-color:' + scope.baremptycolor + ';margin-bottom:4px"></div>' +
+                    '<div ng-mousemove="drag($event)" ng-mousedown="down()" ng-mouseup="up()" orientation="vertical" style="cursor:ns-resize;margin-top:-4px;margin-left:5px;width:15px;height:15px;background-color:' + scope.buttoncolor + ';border-radius:50%;"></div>' +
+                    '<div ng-mousemove="drag($event)" ng-mouseup="up()" ng-click="barClick()"style="cursor:pointer;margin-left:9px;width:5px;height:{{barPiece * fillWidth}}px;background-color:' + scope.barfillcolor + ';margin-bottom:4px"></div>' +
+                    '</div>';
                 }
-                else {
-                    scope.barPiece = (scope.barWidth / (scope.max - scope.min));
-                }
-                //checks to see if there is a orientation attribute if there is set our template to the vertical template
-                if (iAttr.orientation) {
-                    if ('vertical' === iAttr.orientation) {
-                        scope.orientation = true;
-                        myTemplate = '<div onselectstart="return false;" ondragstart="return false;"ng-mouseleave="up()" ng-mousemove="drag($event)"><button ng-click="butIncrease()" ng-keydown="keyBind($event)">+</button>' +
-                        '<div ng-mousemove="drag($event)" ng-mouseup="up()" ng-click="greyClick()"style="cursor:pointer;margin-left:9px;width:5px;height:{{barPiece * emptWidth}}px;background-color:' + scope.baremptycolor + ';margin-bottom:4px"></div>' +
-                        '<div ng-mousemove="drag($event)" ng-mousedown="down()" ng-mouseup="up()" orientation="vertical" style="cursor:ns-resize;margin-top:-4px;margin-left:5px;width:15px;height:15px;background-color:' + scope.buttoncolor + ';border-radius:50%;"></div>' +
-                        '<div ng-mousemove="drag($event)" ng-mouseup="up()" ng-click="barClick()"style="cursor:pointer;margin-left:9px;width:5px;height:{{barPiece * fillWidth}}px;background-color:' + scope.barfillcolor + ';margin-bottom:4px"></div>' +
-                        '<button ng-click="butDecrease()" ng-keydown="keyBind($event)">-</button></div>';
-                    }
-                }
-                else {
-                    //this builds our horizontal template
-                    myTemplate = '<div onselectstart="return false;" ondragstart="return false;" ng-mouseleave="up()"ng-mousemove="drag($event)"><button style="height: 20px" ng-click="butDecrease()" ng-keydown="keyBind($event)">-</button>' +
+                else{
+                    myTemplate = '<div onselectstart="return false;" ondragstart="return false;" ng-mouseleave="up()"ng-mousemove="drag($event)">' +
                     '<div ng-mousemove="drag($event)" ng-mouseup="up()" ng-click="barClick()"style="cursor:pointer;display:inline-block;width:{{barPiece * fillWidth}}px;height:5px;background-color:' + scope.barfillcolor + ';margin-bottom:4px"></div>' +
                     '<div ng-mousemove="drag($event)" ng-mousedown="down()" ng-mouseup="up()" orientation="horizontal" style="cursor:ew-resize;display:inline-block;width:{{butSize}}px;height:{{butSize}}px;background-color:' + scope.buttoncolor + ';border-radius:50%;"></div>' +
                     '<div ng-mousemove="drag($event)" ng-mouseup="up()" ng-click="greyClick()"style="cursor:pointer;display:inline-block;width:{{barPiece * emptWidth}}px;height:5px;background-color:' + scope.baremptycolor + ';margin-bottom:4px"></div>' +
-                    '<button ng-click="butIncrease()" ng-keydown="keyBind($event)">+</button></div>';
+                    '</div>';
                 }
-                //We show our template and then compile it so the DOM knows about our ng functions
-                iElem.html(myTemplate);
-                $compile(iElem.contents())(scope);
-                //create the initial bar
-                scope.makeBar();
-                return;
             }
+            else {
+                //this builds our horizontal template
+                myTemplate = '<div onselectstart="return false;" ondragstart="return false;" ng-mouseleave="up()"ng-mousemove="drag($event)">' +
+                '<div ng-mousemove="drag($event)" ng-mouseup="up()" ng-click="barClick()"style="cursor:pointer;display:inline-block;width:{{barPiece * fillWidth}}px;height:5px;background-color:' + scope.barfillcolor + ';margin-bottom:4px"></div>' +
+                '<div ng-mousemove="drag($event)" ng-mousedown="down()" ng-mouseup="up()" orientation="horizontal" style="cursor:ew-resize;display:inline-block;width:{{butSize}}px;height:{{butSize}}px;background-color:' + scope.buttoncolor + ';border-radius:50%;"></div>' +
+                '<div ng-mousemove="drag($event)" ng-mouseup="up()" ng-click="greyClick()"style="cursor:pointer;display:inline-block;width:{{barPiece * emptWidth}}px;height:5px;background-color:' + scope.baremptycolor + ';margin-bottom:4px"></div>' +
+                '</div>';
+            }
+            //We show our template and then compile it so the DOM knows about our ng functions
+            iElem.html(myTemplate);
+            $compile(iElem.contents())(scope);
+            //create the initial bar
+            scope.makeBar();
+            return;
         }
     }
 });
